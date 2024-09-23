50fce3295b59ddf558332509a8bb9e32c2cac760
Sebastian Sanabria <ssebastiang97@gmail.com>
2024-09-23 11:04:25 -0500

    Update Cors

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,10 @@ export class Server {
     this.app.use( express.json() );
     this.app.use(
       cors({
+        origin: 'https://the-geek-theory-frontend.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true
        })
     );
 
