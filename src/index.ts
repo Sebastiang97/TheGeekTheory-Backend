35c84c5d892ebe40e393f77bc5ec72b197beecac
Sebastian Sanabria <ssebastiang97@gmail.com>
2024-09-22 20:31:51 -0500

    Add node version

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import dotenv from 'dotenv'
 function main() {
   const port = process.env.PORT || 4000;
   console.log({port})
+  console.log(process.env.PORT)
   const server = new Server({
     port: typeof port === "string" ? Number(port) : port,
     routes: AppRoutes.routes
