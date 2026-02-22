9c433b91c01303d4b8f32d7b3e42147ff5c9400e
Sebastian Sanabria <ssebastiang97@gmail.com>
2026-02-22 15:36:29 -0500

    Update whatsapp-web.js 1.25.0

--- src/libs/whatsapp.ts
+++ src/libs/whatsapp.ts
@@ -1,25 +1,6 @@
-import { Client, LocalAuth } from "whatsapp-web.js"
+import { Client } from "whatsapp-web.js"
 // import qrcode from 'qrcode-terminal'
-export const client = new Client({
-  authTimeoutMs: 120000, // 2 minutos
-  // puppeteer: {
-  //   headless: true,
-  //   args: ['--no-sandbox', '--disable-setuid-sandbox'], // útiles en servidores
-  // }
-  authStrategy: new LocalAuth({
-    dataPath: './session-data' // Guarda la sesión aquí
-  }),
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage', // CRÍTICO para entornos con memoria limitada
-      '--disable-accelerated-2d-canvas',
-      '--disable-gpu'
-    ]
-  }
-})
+export const client = new Client({})
 
 export let QR_CODE_DATA = '';
 export let IS_CLIENT_READY = false;
