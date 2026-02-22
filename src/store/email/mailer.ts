873909aee71113dd3d072fed093fd2f907dacb9c
Sebastian Sanabria <ssebastiang97@gmail.com>
2026-02-22 16:57:52 -0500

    UPdate nodemailer

--- src/store/email/mailer.ts
+++ src/store/email/mailer.ts
@@ -1,25 +1,15 @@
 import nodemailer from 'nodemailer'
 
 export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587, // Puerto recomendado para STARTTLS
-    secure: false, // false porque usamos el 587, STARTTLS lo hará seguro después
-    requireTLS: true, // Forzar el uso de TLS
+    host: "mail.smtp2go.com", // Host de SMTP2GO
+    port: 2525, // <--- PUERTO MÁGICO QUE FUNCIONA EN FREE TIER
+    secure: false, // El puerto 2525 usa STARTTLS, no SSL directo
+    requireTLS: true, // Forzar la conexión segura
     auth: {
       user: process.env.MAILER_USERNAME,
       pass: process.env.MAILER_PASSWORD,
+    //   pass: "vucsxmlbjohxmqel",
     },
-    tls: {
-        // No rechazar certificados autofirmados (Gmail no tiene, pero a veces ayuda)
-        rejectUnauthorized: true, // Mantener en 'true' en producción para seguridad
-        minVersion: "TLSv1.2"
-    },
-    // Aumentar timeouts
-    connectionTimeout: 60000, // 60 segundos
-    greetingTimeout: 30000,   // 30 segundos
-    socketTimeout: 60000,      // 60 segundos
-    debug: true, // Activa logs detallados (quitar en producción si no se necesita)
-    logger: true // Para ver logs en la consola
   });
 
   transporter.verify().then(()=>{
